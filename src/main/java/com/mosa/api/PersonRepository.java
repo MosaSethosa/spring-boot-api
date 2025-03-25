7056bdd45cecb431a90c693e0c369e76d5ff00cc
Mosa Sethosa <mosa@Mosas-MacBook-Air.local>
2025-03-25 12:27:43 +0200

    Person repo

---
+++ src/main/java/com/mosa/api/PersonRepository.java
@@ -0,0 +1,8 @@
+package com.mosa.api;
+
+import com.mosa.api.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    Person findByEmail(String email);
+}
