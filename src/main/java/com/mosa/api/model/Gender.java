dc806ee5cff282cce15da0668ca873c77e3d9df9
Mosa Sethosa <mosa@Mosas-MacBook-Air.local>
2025-03-25 12:18:36 +0200

    definig Person model

---
+++ src/main/java/com/mosa/api/model/Gender.java
@@ -0,0 +1,7 @@
+package com.mosa.api.model;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/main/java/com/mosa/api/model/Person.java
@@ -0,0 +1,15 @@
+package com.mosa.api.model;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private Long id;
+    private String name;
+    private LocalDate dateOfBirth;
+    private int age;
+    private Gender gender;
+    private String email;
+    private String photoUrl;
+
+}
